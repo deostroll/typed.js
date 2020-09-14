685fc48cfd6ab073c1c9f5f283420717513a37aa
deostroll <deostroll@gmail.com>
2020-09-14 19:03:03 +0000

    Voyager

---
+++ assets/voyager.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var typed = new Typed('#typed', {
+    stringsElement: '#my-strings',
+    typeSpeed: 20,
+    backSpeed: 5,
+    startDelay: 1000,
+    loop: false,
+    loopCount: Infinity    
+  });
+}, false);
